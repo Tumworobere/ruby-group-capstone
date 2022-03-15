6ddc068f5ad51233879c1c5bb326bff98ad4a653
Furkan Denizhan <84407337+nevisende@users.noreply.github.com>
2022-03-15 12:56:50 +0300

    Update: item class to add setters and detailed methods

--- item.rb
+++ item.rb
@@ -1,18 +1,40 @@
-class Item
-  attr_accessor :genre, :author, :source, :label, :publish_date, :name
-  attr_reader :id, :archived
+require 'date'
 
-  def initialize(genre, author, source, label, publish_date)
-    @id = Math.rand(1..500)
+class Item
+  attr_accessor :genre, :author, :source, :label, :publish_date
+
+  def initialize(publish_date)
+    @id = rand(1..5000)
+    @publish_date = Date.parse(publish_date)
+    @archived = false
+  end
+  def add_genre(genre)
     @genre = genre
-    @author = author
-    @source = source
-    @label = label
-    @publish_date = publish_date
-    @achived = false
+    genre.items << self
   end
 
-  def self.can_be_achived?; end
+  def add_source(source)
+    @source = source
+    source.items << self
+  end
 
-  def move_to_achive; end
+  def add_author(author)
+    @author = author
+    author.items << self
+  end
+
+  def add_label(label)
+    @label = label
+    label.items << self
+  end
+
+  def move_to_archive
+    can_be_archived? && @archived = true
+  end
+
+  private
+
+  def can_be_archived?
+    Time.now.year - @publish_date.year > 10
+  end  
 end

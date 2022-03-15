cf7ec27417db15a055be2e852c715a018f2217dc
Furkan Denizhan <84407337+nevisende@users.noreply.github.com>
2022-03-15 11:34:39 +0300

    Feat: create author class
    - add first_name, last_name instance variables
    - create add_item method it push the item to items array variable of instance

---
+++ author.rb
@@ -0,0 +1,14 @@
+class Author
+  attr_accessor :first_name, :last_name
+
+  def initialize(first_name, last_name)
+    @id = Random.rand(1..500)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+  end
+end

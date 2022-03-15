b259102e75a161c576ad62dd3b5177572e6ad675
Annah Tomoro <annaht.annah@gmail.com>
2022-03-15 15:02:29 +0300

    Update store.rb

--- store.rb
+++ store.rb
@@ -1,5 +1,18 @@
 require 'json'
+require_relative './music_album'
+require_relative './music_genre'
 
 class Store
- 
-end
+    def list_all_music_album
+        puts 'Music Albums'
+        @music_albums.each do |music_album|
+          puts "Name: #{music_album.name}, Publish Date: #{music_album.publish_date}, On Spotify: #{music_album.on_spotify}"
+        end
+      end
+
+      def list_all_genres
+        puts 'Genres'
+        @genres.each do |genre|
+          puts "Name: #{genre.name}"
+        end
+      end

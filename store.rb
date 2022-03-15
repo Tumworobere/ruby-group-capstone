3ec454a937b2927896f884b0c29bb5e476708cda
unknown <azeemolatunji@gmail.com>
2022-03-15 11:28:33 +0100

    add method for list book and label to store.rb

--- store.rb
+++ store.rb
@@ -1,5 +1,19 @@
 require 'json'
+require_relative './book'
+require_relative './label'
 
 class Store
- 
+  def list_books
+    puts 'There are no books yet! Please add books.' if @books.empty?
+    @books.each do |book|
+      puts "Title: #{book.name}, Publish Date: #{book.publish_date}, cover_state: #{book.cover_state}"
+    end
+  end
+
+  def list_labels
+    puts 'There are no labels yet!' if @labels.empty?
+    @labels.each do |label|
+      puts "Label name: #{label.title}"
+    end
+  end
 end

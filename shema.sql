dd28e4b67fbbbc213b54f8fa5a4cc7f3569470a5
Furkan Denizhan <84407337+nevisende@users.noreply.github.com>
2022-03-16 10:46:18 +0300

    Feat: create the postgresql shema

---
+++ shema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "item"(
+    "id" INTEGER NOT NULL,
+    "genre" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "source" TEXT NOT NULL,
+    "label" TEXT NOT NULL,
+    "publish_date" DATE NOT NULL,
+    "archived" BOOLEAN NOT NULL
+);
